dafe55be6bd9ddce644058cc4415f106ed92f82b
Mahno <weudewiudwasdadsieud@mail.ru>
2023-10-27 16:03:58 +0300

    lib boost

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,12 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <iostream>
 
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+using tcp = boost::asio::ip::tcp;
 int main ()
 
 {
